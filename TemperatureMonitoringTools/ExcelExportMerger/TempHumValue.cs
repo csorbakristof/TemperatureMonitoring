852fd2cf9eac5c1d16ec6594e4042d60b9968952
kristof <csorba.kristof@vik.bme.hu>
2024-09-10 14:08:19 +0200

    Initial commit of ExcelExportMerger

---
+++ TemperatureMonitoringTools/ExcelExportMerger/TempHumValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelExportMerger
+{
+    internal class TempHumValue
+    {
+        public string DeviceName { get; set; }
+        public DateTime Timestamp { get; set; }
+        public float? Temperature { get; set; }
+        public float? Humidity { get; set; }
+        public string Comment { get; set; }
+    }
+}
